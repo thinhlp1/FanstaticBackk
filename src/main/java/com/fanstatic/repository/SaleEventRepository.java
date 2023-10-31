5db336a71023d34739bacf6b287008c8ded37a44
phuoctaiismee <113773744+phuoctaiismee@users.noreply.github.com>
2023-11-01 00:42:00 +0700

    [API] Done Customer and SaleEvent

--- src/main/java/com/fanstatic/repository/SaleEventRepository.java
+++ src/main/java/com/fanstatic/repository/SaleEventRepository.java
@@ -3,6 +3,17 @@ package com.fanstatic.repository;
 import com.fanstatic.model.SaleEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 
 public interface SaleEventRepository extends JpaRepository<SaleEvent, Integer> {
+    public Optional<List<SaleEvent>> findAllByActiveIsTrue();
+
+    public Optional<List<SaleEvent>> findAllByActiveIsFalse();
+
+    public Optional<SaleEvent> findByIdAndActiveIsTrue(int id);
+
+    public Optional<SaleEvent> findByIdAndActiveIsFalse(int id);
+
 }
