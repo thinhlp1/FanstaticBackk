e9a96bee74a391dab556affc7cab76c095559d5d
thinhlp1 <alanthinh3@gmail.com>
2023-11-03 08:51:14 +0700

    --new-api auth

--- src/main/java/com/fanstatic/controller/auth/AuthenticationController.java
+++ src/main/java/com/fanstatic/controller/auth/AuthenticationController.java
@@ -2,16 +2,22 @@ package com.fanstatic.controller.auth;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fanstatic.dto.ResponseDTO;
+import com.fanstatic.dto.auth.ConfirmOtpDTO;
+import com.fanstatic.dto.auth.ConfirmPasswordDTO;
+import com.fanstatic.dto.auth.LoginAccountDTO;
 import com.fanstatic.dto.auth.LoginDTO;
+import com.fanstatic.dto.auth.LoginPasswordDTO;
 import com.fanstatic.service.authentication.AuthenticationService;
 import com.fanstatic.util.ResponseUtils;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 
 @Controller
@@ -21,11 +27,52 @@ public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
-    @PostMapping("/login")
+    @PostMapping("/employee/login")
     @ResponseBody
-    public ResponseEntity<ResponseDTO> login(@RequestBody LoginDTO loginDTO) {
+    public ResponseEntity<ResponseDTO> login(@RequestBody LoginAccountDTO loginDTO) {
         ResponseDTO reponseDTO = authenticationService.login(loginDTO);
         return ResponseUtils.returnReponsetoClient(reponseDTO);
     }
 
+    @PostMapping("/login")
+    @ResponseBody
+    public ResponseEntity<ResponseDTO> login(@RequestBody @Valid LoginDTO loginDTO) {
+        ResponseDTO reponseDTO = authenticationService.login(loginDTO);
+        return ResponseUtils.returnReponsetoClient(reponseDTO);
+    }
+
+    @PostMapping("/login-password")
+    @ResponseBody
+    public ResponseEntity<ResponseDTO> login(@RequestBody @Valid LoginPasswordDTO loginDTO) {
+        ResponseDTO reponseDTO = authenticationService.loginPassword(loginDTO);
+        return ResponseUtils.returnReponsetoClient(reponseDTO);
+    }
+
+    @PostMapping("/confirm-otp")
+    @ResponseBody
+    public ResponseEntity<ResponseDTO> confirmOTP(@RequestBody @Valid ConfirmOtpDTO confirmOtpDTO) {
+        ResponseDTO reponseDTO = authenticationService.confirmOTP(confirmOtpDTO);
+        return ResponseUtils.returnReponsetoClient(reponseDTO);
+    }
+
+    @GetMapping("/resend-otp")
+    @ResponseBody
+    public ResponseEntity<ResponseDTO> resendOTP() {
+        ResponseDTO reponseDTO = authenticationService.resendOTP();
+        return ResponseUtils.returnReponsetoClient(reponseDTO);
+    }
+
+    @PostMapping("/confirm-password")
+    @ResponseBody
+    public ResponseEntity<ResponseDTO> confirmPassword(@RequestBody @Valid ConfirmPasswordDTO confirmPasswordDTO) {
+        ResponseDTO reponseDTO = authenticationService.confirmPassword(confirmPasswordDTO);
+        return ResponseUtils.returnReponsetoClient(reponseDTO);
+    }
+
+    @PostMapping("/forget/confirm-password")
+    @ResponseBody
+    public ResponseEntity<ResponseDTO> forgetConfirmPassword(@RequestBody @Valid ConfirmPasswordDTO confirmPasswordDTO) {
+        ResponseDTO reponseDTO = authenticationService.forgetConfirmPassword(confirmPasswordDTO);
+        return ResponseUtils.returnReponsetoClient(reponseDTO);
+    }
 }
