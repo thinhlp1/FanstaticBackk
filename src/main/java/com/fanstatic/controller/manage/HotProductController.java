95c9c17cfe46d333509811b35616023e0236821a
thinhlp1 <alanthinh3@gmail.com>
2023-11-23 14:56:10 +0700

    -API hot product

---
+++ src/main/java/com/fanstatic/controller/manage/HotProductController.java
@@ -0,0 +1,13 @@
+package com.fanstatic.controller.manage;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@RequestMapping("/api/manage/hotproduct")
+@AllArgsConstructor
+public class HotProductController {
+    
+}
