c0b184041866deb4f9aa639cc6cba8b217a69954
ltkien2003 <138638070+ltkien2003@users.noreply.github.com>
2023-12-10 21:18:03 +0700

    Update WebSecurityConfig.java

--- src/main/java/com/fanstatic/config/security/WebSecurityConfig.java
+++ src/main/java/com/fanstatic/config/security/WebSecurityConfig.java
@@ -62,7 +62,7 @@ public class WebSecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://20.198.255.137:3000", "https://4455-20-198-255-137.ngrok-free.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://149.28.143.65:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT", "OPTIONS", "PATCH", "DELETE"));
         configuration.setAllowCredentials(true);
         configuration.addAllowedHeader("*"); // Thêm dòng này
