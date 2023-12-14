e4121544f3edaa1b770cefb2be9f9a261b3f868f
thinhlp1 <alanthinh3@gmail.com>
2023-12-14 20:33:56 +0700

    --fix remove transaction in update order

--- src/main/java/com/fanstatic/config/system/PointProgramConfig.java
+++ src/main/java/com/fanstatic/config/system/PointProgramConfig.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class PointProgramConfig extends ResponseDataDTO {
-    private Long minPoint;
+    private Long minPrice;
     private Long maxPoint;
     private ConvertRate moneyToPoint;
     private ConvertRate pointToMoney;
